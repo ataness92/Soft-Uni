daa410dff0e3e249b6b91eb1626e903079b4d2af
ataness92 <57792148+ataness92@users.noreply.github.com>
2023-10-10 15:16:55 +0300

    Update invert_values.py

--- python_fundamentals/lists_basics/invert_values.py
+++ python_fundamentals/lists_basics/invert_values.py
@@ -1,1 +1,9 @@
 string_numbers = input()
+string_numbers = string_numbers.split(' ')
+new_string = list()
+#print(list(string_numbers.split(' ')))
+
+for i in string_numbers:
+    new_string.append(int(i)*(-1))
+
+print(new_string)
