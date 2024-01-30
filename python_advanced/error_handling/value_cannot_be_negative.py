7900d497dab02de1c22a4629b36bc0169f8353ae
ataness92 <57792148+ataness92@users.noreply.github.com>
2024-01-30 18:15:58 +0200

    Update value_cannot_be_negative.py

--- python_advanced/error_handling/value_cannot_be_negative.py
+++ python_advanced/error_handling/value_cannot_be_negative.py
@@ -1,10 +1,9 @@
-class ValueTooSmallError(Exception):
+class ValueCannotBeNegativeError(Exception):
     """Raised when the input value is too small"""
     pass
 
-amount_to_send = float(input())
+for _ in range(5):
+    number = int(input())
 
-if amount_to_send <= 0 < 1:
-    raise ValueTooSmallError("Value is too low")
-
-print("Pass")
+    if number < 0:
+        raise ValueCannotBeNegativeError("Value Cannot Be Negative")
