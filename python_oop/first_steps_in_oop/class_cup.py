b120802ebcf897337e48b50d56638fcfb675e6fa
ataness92 <57792148+ataness92@users.noreply.github.com>
2024-02-19 17:37:31 +0200

    Create class_cup.py

---
+++ python_oop/first_steps_in_oop/class_cup.py
@@ -0,0 +1,17 @@
+class Cup:
+    
+    def __init__(self, size, quantity):
+        self.size = size
+        self.quantity = quantity
+
+    
+    def fill(self, quantity):
+        if self.size > self.quantity+quantity:
+            self.quantity += quantity
+
+    def status(self):
+        return self.size-self.quantity
+
+cup = Cup(10, 4)
+
+print(cup.status())
