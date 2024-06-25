d14c4998f405ab37a5565549712580dee856e9f7
atanas <ataness92@gmail.com>
2024-06-25 22:05:56 +0300

    New Updates

---
+++ python_oop/exam_preparation/december_08_2023/project/fish/predatory_fish.py
@@ -0,0 +1,13 @@
+from project.fish.base_fish import BaseFish
+
+
+class PredatoryFish(BaseFish):
+    TIME = 90
+
+    def __init__(self, name, points):
+        super().__init__(name, points, self.TIME)
+
+    def fish_details(self):
+        return f"{self.__class__.__name__}: {self.name} [Points: {self.points}, Time to Catch: {self.time_to_catch} seconds]"
+
+    
---
+++ python_oop/unit_testing/mammal/test/test_mammal.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, main
+
+from mammal import Mammal
+
+
+class TestMammal(TestCase):
+
+    def setUp(self) -> None:
+        self.mammal = Mammal("Nasko", "example", "muu")
+
+    def test_init(self):
+        self.assertEqual("Nasko", self.mammal.name)
+        self.assertEqual("example", self.mammal.type)
+        self.assertEqual("muu", self.mammal.sound)
+        self.assertEqual("animals", self.mammal.get_kingdom())
+
+    def test_make_sound(self):
+        self.assertEqual("Nasko makes muu", self.mammal.make_sound())
+
+    def test_info(self):
+        self.assertEqual(f"Nasko is of type example", self.mammal.info())
+
+if __name__ == "__main__":
+    main()
